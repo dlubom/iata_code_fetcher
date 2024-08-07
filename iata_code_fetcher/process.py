8183b997b085eb5adda9a68f21b4db345c9ee8f2
Darek Lubomski <darek.lubomski@gmail.com>
2024-08-07 16:18:40 +0200

    one file for process

--- iata_code_fetcher/process.py
+++ iata_code_fetcher/process.py
@@ -9,6 +9,20 @@ def process_airport_data(file_path):
     df_deduplicated = df.unique()
 
     result = df_deduplicated.rename(
+        {"3-letter location code": "iata", "City Name": "city_name", "Airport Name": "airport_name"}
+    )
+
+    result = result.sort(["iata", "city_name"])
+
+    return result
+
+
+def process_carrier_data(file_path):
+    df = pl.read_ndjson(file_path)
+
+    df_deduplicated = df.unique()
+
+    result = df_deduplicated.rename(
         {
             "2-letter code": "iata",
             "Country / Territory": "country_or_territory",
@@ -22,17 +36,24 @@ def process_airport_data(file_path):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <path_to_jsonl_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <air|carrier> <path_to_jsonl_file>")
         sys.exit(1)
 
-    file_path = sys.argv[1]
+    mode = sys.argv[1]
+    file_path = sys.argv[2]
 
     if not os.path.exists(file_path):
         print(f"File {file_path} does not exist.")
         sys.exit(1)
 
-    result = process_airport_data(file_path)
+    if mode == "air":
+        result = process_airport_data(file_path)
+    elif mode == "carrier":
+        result = process_carrier_data(file_path)
+    else:
+        print("Invalid mode. Use 'air' or 'carrier'.")
+        sys.exit(1)
 
     output_path = os.path.splitext(file_path)[0] + "_processed.jsonl"
     result.write_ndjson(output_path)
